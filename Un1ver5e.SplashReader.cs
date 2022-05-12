516124cfee57b21b54906257bdfb2f902adcf7bc
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-05-12 19:31:21 +0500

    Moved splash reading to a file.

---
+++ Un1ver5e.SplashReader.cs
@@ -0,0 +1,8 @@
+﻿namespace Un1ver5e.Bot
+{
+    public static class SplashReader
+    {
+        private static readonly string path = Statics.DataFolderPath + "/Splashes.txt";
+        public static string GetSplash() => File.ReadAllLines(path).GetRandomElement();
+    }
+}
